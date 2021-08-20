29273aea7b156ac7ae0f3b49f425b9e36a0f3ce1
Lawyerd <jun12376@gmail.com>
2021-08-20 14:41:21 +0900

    List 데이터 수정

--- src/store/list.js
+++ src/store/list.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
 import cryptoRandomString from "crypto-random-string";
+import _find from "lodash/find";
+import _remove from "lodash/remove";
 
 const crypto = () => {
   return cryptoRandomString({ length: 10 });
@@ -12,10 +14,10 @@ const repoLists = JSON.parse(window.localStorage.getItem("lists")) || [];
 const _lists = writable(repoLists);
 // _를 붙이는 이유, js파일 내부에서만 사용할 것이라서
 _lists.subscribe($lists => {
+  // subscribe하면 값(_lists)이 변할 때마다 동작함
   window.localStorage.setItem("lists", JSON.stringify($lists));
   // setItem(데이터의 이름, 데이터의 값)
   // localStroage에는 문자 데이터만 저장 가능, 객체를 문자로 바꾼다.
-  console.log();
 });
 
 export const lists = {
@@ -35,6 +37,32 @@ export const lists = {
       });
       return $lists;
       // 반환함으로써 $lists를 update한다.
+    });
+  },
+
+  edit(payload) {
+    const { listId, title } = payload;
+    _lists.update($lists => {
+      // let foundList = $lists.find(l => {
+      //   return l.id === listId;
+      // });
+      // lodash라는 모듈을 사용해서 좀 더 간단하게 만들어주자
+      const foundList = _find($lists, { id: listId });
+      foundList.title = title;
+      return $lists;
+    });
+  },
+
+  remove(payload) {
+    const { listId } = payload;
+    _lists.update($lists => {
+      // const filteredList = $lists.filter(el => {
+      //   return el.id !== listId;
+      // });
+
+      // return filteredList;
+      _remove($lists, { id: listId });
+      return $lists;
     });
   },
 };
